25ac20fde76e89eb6adffb7a76b78cfd542cf098
esantanamat <esantanamat@gmail.com>
2025-04-05 23:53:59 -0400

    succesfully containerez + deployed api within localhost container

--- 3-Multi-Container-Application/app/config.py
+++ 3-Multi-Container-Application/app/config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_USER = os.getenv("DB_USER")
-POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
-POSTGRES_HOST = os.getenv("DB_HOST")
-POSTGRES_PORT = os.getenv("DB_PORT", "5432") 
-POSTGRES_DB = os.getenv("DB_NAME")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432") 
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
