c13cea0fee95f6a1ba842de510be5cb4e39b3be8
esantanamat <esantanamat@gmail.com>
2025-04-07 16:12:45 -0400

    fixed ssh perm for github

--- 3-Multi-Container-Application/infrastructure/main.tf
+++ 3-Multi-Container-Application/infrastructure/main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "ec2_security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description= "HTTP"
