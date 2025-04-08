d0b4dda604911353623f6b4c3071992dcadfa412
esantanamat <esantanamat@gmail.com>
2025-04-07 20:44:16 -0400

    whoops forgot to launch the postgresql container

--- 3-Multi-Container-Application/infrastructure/main.tf
+++ 3-Multi-Container-Application/infrastructure/main.tf
@@ -30,6 +30,14 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["${var.my_ip}/32"]
   }
 
+  ingress {
+    description= "allow traffic "
+    from_port=5000
+    to_port=5000
+    protocol="tcp"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
